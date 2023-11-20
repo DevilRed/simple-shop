2e4089172c7f7c7c077791dcf6f9f13f26972327
Christian <chrisdaq@gmail.com>
2023-11-20 18:51:24 -0400

    show proper title

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,15 @@
+import { useParams } from "react-router-dom";
 import { Item } from "./Item";
 
 export const ItemList = ({ products }) => {
+  let category = useParams().category;
+  console.log(category);
+  if (typeof category === "undefined") {
+    category = "Products";
+  }
   return (
     <div className="container">
-      <h2 className="main-title">Products</h2>
+      <h2 className="main-title">{category}</h2>
       <div className="productos">
         {products.map((prod) => (
           <Item key={prod.id} product={prod} />
