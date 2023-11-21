2ae139ca0e16259d468771517eaf45fdaf348e43
Christian <chrisdaq@gmail.com>
2023-11-21 17:57:16 -0400

    memoize item detail

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { ItemDetail } from "./ItemDetail";
 import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
@@ -7,12 +7,15 @@ import { db } from "../firebase/config";
 export const ItemDetailContainer = () => {
   const [item, setItem] = useState(null);
   const id = useParams().id;
-
-  useEffect(() => {
+  const getItem = useMemo(() => {
     const docRef = doc(db, "products", id);
     getDoc(docRef).then((resp) => {
       setItem({ ...resp.data(), id: resp.id });
     });
+  }, [id]);
+
+  useEffect(() => {
+    getItem;
   }, [id]);
 
   return <div>{item && <ItemDetail item={item} />}</div>;
