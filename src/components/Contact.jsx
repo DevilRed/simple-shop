f9dcdf41afd4ccc2e7045d8fd3867cd5faef51a3
Christian <chrisdaq@gmail.com>
2023-11-22 16:51:43 -0400

    fix form styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,59 +7,70 @@ export const Contact = () => {
     console.log(data);
   };
   return (
-    <div className="container">
-      <h1 className="main-title">Contact</h1>
-      <form className="formulario" onSubmit={handleSubmit(sendData)} noValidate>
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="name"
-            {...register("name", {
-              required: "name is required",
-            })}
-          />
-          <p className="error">{errors.name?.message}</p>
+    <div>
+      <div className="container">
+        <div className="col-md-6 mx-auto">
+          <h1 className="main-title">Contact</h1>
+          <form
+            className="formulario d-flex justify-content-center"
+            onSubmit={handleSubmit(sendData)}
+            noValidate
+          >
+            <div className="form-group w-100">
+              <input
+                type="text"
+                placeholder="name"
+                className="form-control"
+                {...register("name", {
+                  required: "name is required",
+                })}
+              />
+              <p className="error">{errors.name?.message}</p>
+            </div>
+            <div className="form-group w-100">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="email"
+                {...register("email", {
+                  pattern: {
+                    value:
+                      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: "Invalid email format",
+                  },
+                  required: "email is required",
+                })}
+              />
+              <p className="error">{errors.email?.message}</p>
+            </div>
+            <div className="form-group w-100">
+              <input
+                type="phone"
+                className="form-control"
+                placeholder="phone number"
+                {...register("phone")}
+              />
+              <p className="error">{errors.phone?.message}</p>
+            </div>
+            <div className="form-group w-100">
+              <textarea
+                name="body"
+                className="form-control"
+                cols="30"
+                rows="10"
+                {...register("body", {
+                  required: "text body is required",
+                })}
+              ></textarea>
+              <p className="error">{errors.body?.message}</p>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Send
+            </button>
+          </form>
         </div>
-        <div className="form-control">
-          <input
-            type="email"
-            placeholder="email"
-            {...register("email", {
-              pattern: {
-                value:
-                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                message: "Invalid email format",
-              },
-              required: "email is required",
-            })}
-          />
-          <p className="error">{errors.email?.message}</p>
-        </div>
-        <div className="form-control">
-          <input
-            type="phone"
-            placeholder="phone number"
-            {...register("phone")}
-          />
-          <p className="error">{errors.phone?.message}</p>
-        </div>
-        <div className="form-control">
-          <textarea
-            name="body"
-            cols="30"
-            rows="10"
-            {...register("body", {
-              required: "text body is required",
-            })}
-          ></textarea>
-          <p className="error">{errors.body?.message}</p>
-        </div>
-        <div className="form-control">
-          <button type="submit" className="enviarx">
-            Send
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
