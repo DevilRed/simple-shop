069af76adbb1f1b2399c94bf2764ed7862b1e5a4
Christian <chrisdaq@gmail.com>
2023-11-22 15:56:34 -0400

    show validation message on the frontend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 
 export const Contact = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
   const sendData = (data) => {
     console.log(data);
   };
@@ -9,36 +10,55 @@ export const Contact = () => {
     <div className="container">
       <h1 className="main-title">Contact</h1>
       <form className="formulario" onSubmit={handleSubmit(sendData)} noValidate>
-        <input
-          type="text"
-          placeholder="name"
-          {...register("name", {
-            required: "name is required",
-          })}
-        />
-        <input
-          type="email"
-          placeholder="email"
-          {...register("email", {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
-        <input type="phone" placeholder="phone number" {...register("phone")} />
-        <textarea
-          name="body"
-          cols="30"
-          rows="10"
-          {...register("body", {
-            required: "text body is required",
-          })}
-        ></textarea>
-        <button type="submit" className="enviarx">
-          Send
-        </button>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="name"
+            {...register("name", {
+              required: "name is required",
+            })}
+          />
+          <p className="error">{errors.name?.message}</p>
+        </div>
+        <div className="form-control">
+          <input
+            type="email"
+            placeholder="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              required: "email is required",
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+        <div className="form-control">
+          <input
+            type="phone"
+            placeholder="phone number"
+            {...register("phone")}
+          />
+          <p className="error">{errors.phone?.message}</p>
+        </div>
+        <div className="form-control">
+          <textarea
+            name="body"
+            cols="30"
+            rows="10"
+            {...register("body", {
+              required: "text body is required",
+            })}
+          ></textarea>
+          <p className="error">{errors.body?.message}</p>
+        </div>
+        <div className="form-control">
+          <button type="submit" className="enviarx">
+            Send
+          </button>
+        </div>
       </form>
     </div>
   );
