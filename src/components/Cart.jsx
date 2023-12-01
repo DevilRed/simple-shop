d7da03e80fab0ce4c02bb9937ba13ea2ea9e78f0
Christian <chrisdaq@gmail.com>
2023-12-01 17:32:50 -0400

    adapt cart to context changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,23 +9,36 @@ export const Cart = () => {
   return (
     <div className="container">
       <h1 className="main-title">Cart</h1>
-      {cart.map((prod) => (
-        <div key={prod.id}>
-          <br />
-          <h3>{prod.titulo}</h3>
-          <p>Precio unit: ${prod.precio}</p>
-          <p>Precio total: ${prod.precio * prod.cantidad}</p>
-          <p>Cant: {prod.cantidad}</p>
-          <br />
-        </div>
-      ))}
-      {cart.length > 0 ? (
+      {typeof cart[0] !== "undefined" &&
+        cart[0].map((prod) => {
+          if (Array.isArray(prod) === false) {
+            return (
+              <div key={prod.id}>
+                <br />
+                <h3>{prod.titulo}</h3>
+                <p>Precio unit: ${prod.precio}</p>
+                <p>
+                  Precio total: $
+                  {isNaN(totalPrice()) !== false
+                    ? prod.precio * prod.cantidad
+                    : 0}
+                </p>
+                <p>Cant: {prod.cantidad}</p>
+                <br />
+              </div>
+            );
+          }
+        })}
+
+      {typeof cart[0] !== "undefined" && cart[0].length > 1 ? (
         <>
-          <h2>Total price: ${totalPrice()}</h2>
+          <h2>
+            Precio total: ${isNaN(totalPrice()) === false ? totalPrice() : 0}
+          </h2>
           <button onClick={handleEmptyCart}>Empty Cart</button>
         </>
       ) : (
-        <h5>Cart is empty</h5>
+        <h2>Your cart is empty :(</h2>
       )}
     </div>
   );
